010268e946248c3b481638256fd1bad183cbce05
mitevdimitar <mitev.dimitar@gmail.com>
2020-04-28 14:25:53 +0300

    Link to linkedin added

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -5,7 +5,8 @@ function Footer() {
     return (
         <footer id="site-footer">
             <p>My Favorite PL Player / 
-                <a className="linkedin-link" href="https://www.linkedin.com/in/mitev-dimitar/" target="_blank" rel="noopener noreferrer">Dimitar Mitev</a></p>
+                <a className="linkedin-link" href="https://www.linkedin.com/in/mitev-dimitar/" target="_blank" rel="noopener noreferrer">Dimitar Mitev</a>
+            </p>
         </footer>
     )
 }
