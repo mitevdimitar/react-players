16728456693a418f45cbb2910cfd39e50ee0e357
mitevdimitar <mitev.dimitar@gmail.com>
2020-05-03 13:54:14 +0300

    Created an optimized production build

---
+++ build/precache-manifest.6cb489e28d26eb72228bd1ef800a8a7d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b6b35372dc8f73ff820d0b740d7053",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85a5c28dbd56781d1b11",
+    "url": "/static/css/main.66fdb9b7.chunk.css"
+  },
+  {
+    "revision": "09c8ba37040d6f59aab6",
+    "url": "/static/js/2.580f2cb3.chunk.js"
+  },
+  {
+    "revision": "b4ef5c08fb3000fc57ecdbc8205c5624",
+    "url": "/static/js/2.580f2cb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a5c28dbd56781d1b11",
+    "url": "/static/js/main.1881b5f7.chunk.js"
+  },
+  {
+    "revision": "af43a8a193db17ea48b2",
+    "url": "/static/js/runtime-main.ffd339f4.js"
+  },
+  {
+    "revision": "6340b0b68915136a080f1b99d6c35566",
+    "url": "/static/media/error-404.6340b0b6.png"
+  },
+  {
+    "revision": "b5d2265c829d61914a981b4b6bf4faf0",
+    "url": "/static/media/soccer.b5d2265c.jpg"
+  },
+  {
+    "revision": "880d866afbdd20df54ef4121fc9d99c7",
+    "url": "/static/media/texture-pattern-light-background-gradient-light-texture_1920x1200_sc.880d866a.jpg"
+  },
+  {
+    "revision": "e30e9fb11022bbf5479662e5c8860dee",
+    "url": "/static/media/under-construction.e30e9fb1.jpg"
+  }
+]);
