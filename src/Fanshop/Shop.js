42f1222a7dae44c1682858e3e876329fd58fbdaf
mitevdimitar <mitev.dimitar@gmail.com>
2020-06-01 17:07:58 +0300

    shop component created

---
+++ src/Fanshop/Shop.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Shop.css'
+
+class Shop extends Component {
+
+    render() {
+        return (
+            <div class="shop-container">
+                <h1>Shop</h1>
+            </div>
+        )
+    }
+}
+
+export default Shop;
