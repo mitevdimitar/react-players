7cea36ca25663443e8aafca5413d3e59f7dafed0
mitevdimitar <mitev.dimitar@gmail.com>
2020-06-04 15:30:10 +0300

    product layout created

--- src/Fanshop/Shop.js
+++ src/Fanshop/Shop.js
@@ -1,12 +1,29 @@
 import React, {Component} from 'react';
+import ball from '../img/ball.png';
 import './Shop.css'
 
 class Shop extends Component {
 
     render() {
         return (
-            <div class="shop-container">
+            <div className="shop-container">
                 <h1>Shop</h1>
+                <div className="products-container">
+                    <div className="product">
+                        <h3>
+                            Product 1
+                        </h3>
+                        <div>
+                            <img src={ball} alt="ball" />
+                        </div>
+                    </div>
+                    <div className="product">
+                        Product 2
+                    </div>
+                    <div className="product">
+                        Product 3
+                    </div>
+                </div>
             </div>
         )
     }
