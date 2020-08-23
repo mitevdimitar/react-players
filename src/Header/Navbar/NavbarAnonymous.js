f6bde230aaf5c0d383d87fe3f3914e665ff6fdb5
mitevdimitar <mitev.dimitar@gmail.com>
2020-08-23 13:44:35 +0300

    rankings made public

--- src/Header/Navbar/NavbarAnonymous.js
+++ src/Header/Navbar/NavbarAnonymous.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavbarAnonymous() {
     return (
             <section className="navbar-anonymous">
-                <ul>
-                    <li>
-                        <a className="nav-button" href="/register">
-                            <i className="fas fa-user-plus"></i> Register
-                        </a>
-                    </li>
-                    <li>
-                        <a className="nav-button" href="/login">
-                            <i className="fas fa-sign-in-alt"></i> Login
-                        </a>
-                    </li>
-                </ul>
+                 <div className="nav-first-line">
+                        <div className="first-bar">
+                            <Link className="button" to="/dashboard">Rankings</Link>
+                        </div>
+                        <div className="second-bar">
+                        <ul>
+                                <li>
+                                    <a className="nav-button" href="/register">
+                                        <i className="fas fa-user-plus"></i> Register
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="nav-button" href="/login">
+                                        <i className="fas fa-sign-in-alt"></i> Login
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
             </section>
     )
 }
