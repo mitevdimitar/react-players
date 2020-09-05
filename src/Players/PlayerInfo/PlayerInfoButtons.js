ef6d0b86396775b55e6c8ab14f25fc7ba3f717a2
mitevdimitar <mitev.dimitar@gmail.com>
2020-09-05 15:01:38 +0300

    prompt user to log in from player details

--- src/Players/PlayerInfo/PlayerInfoButtons.js
+++ src/Players/PlayerInfo/PlayerInfoButtons.js
@@ -1,19 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './PlayerInfoButtons.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { UserContext } from '../../ContextWrapper';
 import { withRouter } from "react-router";
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 
 function PlayerInfoButtons({player, isMyPlayer, history}) {
-    const auth = useContext(UserContext)
+    const auth = useContext(UserContext);
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const handleDetailsButton = () => {
 
         if (auth.user) {
             history.push(`/player-details/${player.getId}`)
         } else {
-            console.log("not logged in")
+            setOpen(true);
         }
     }
 
@@ -36,6 +48,28 @@ function PlayerInfoButtons({player, isMyPlayer, history}) {
                             Details
                         </a>
                         <FontAwesomeIcon icon={faHeart} /> <span>{player.likes}</span>
+                        <Dialog
+                            open={open}
+                            onClose={handleClose}
+                            aria-labelledby="alert-not-logged-in"
+                            aria-describedby="alert-prompt-log-in"
+                        >
+                            <DialogTitle id="alert-not-logged-in">{"You are not logged in!"}</DialogTitle>
+                            <DialogContent>
+                            <DialogContentText id="alert-prompt-log-in">
+                                Log in, so you could view player details and vote for your favorite player or 
+                                add it if you can't see him among the nominees
+                            </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                            <Button onClick={() => handleClose} color="primary">
+                                Leave
+                            </Button>
+                            <Button onClick={() => history.push('/login')} color="primary" autoFocus>
+                                Login
+                            </Button>
+                            </DialogActions>
+                        </Dialog>
                     </div>
                 )
             }
