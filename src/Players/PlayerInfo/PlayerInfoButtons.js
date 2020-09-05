778556feef772cf3d40bc18e8c5565e5463a574a
mitevdimitar <mitev.dimitar@gmail.com>
2020-09-05 14:52:16 +0300

    component PlayerInfoButtons made functional

--- src/Players/PlayerInfo/PlayerInfoButtons.js
+++ src/Players/PlayerInfo/PlayerInfoButtons.js
@@ -1,60 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './PlayerInfoButtons.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Redirect } from "react-router-dom";
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { UserContext } from '../../ContextWrapper';
 import { withRouter } from "react-router";
 
-class PlayerInfoButtons extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            likes: this.props.player.likes,
-            redirect: null
-        };
-    }
-    static contextType = UserContext;
+function PlayerInfoButtons({player, isMyPlayer, history}) {
+    const auth = useContext(UserContext)
 
+    const handleDetailsButton = () => {
 
-   
-
-handleDetailsButton = () => {
-    if (this.context.user) {
-        this.props.history.push(`/player-details/${this.props.player.getId}`)
-    } else {
-        console.log("not logged in")
-    }
-}
-
-    render() {
-        if (this.state.redirect) {
-            return <Redirect to={this.state.redirect} />
-          }
-        if (this.props.isMyPlayer) {
-            return (
-                <div ref={this.listRef} className="player-info">
-                    <a className="details-button" href={"/player-details/" + this.props.player.getId}>
-                        Edit
-                    </a>
-                    <a className="details-button" href={"/delete/" + this.props.player.getId}>
-                        Delete
-                    </a>
-                </div>
-            )
+        if (auth.user) {
+            history.push(`/player-details/${player.getId}`)
         } else {
-            return (
-                <div className="player-info">
-                    <a className="details-button" onClick={this.handleDetailsButton}>
-                        Details
-                    </a>
-                    <FontAwesomeIcon icon={faHeart} /> <span>{this.state.likes}</span>
-                </div>
-            )
+            console.log("not logged in")
         }
     }
+
+    const renderPlayers = () => {
+            if (isMyPlayer) {
+                return (
+                    <div /* ref={this.listRef}  */className="player-info">
+                        <a className="details-button" href={"/player-details/" + player.getId}>
+                            Edit
+                        </a>
+                        <a className="details-button" href={"/delete/" + player.getId}>
+                            Delete
+                        </a>
+                    </div>
+                )
+            } else {
+                return (
+                    <div className="player-info">
+                        <a className="details-button" onClick={handleDetailsButton}>
+                            Details
+                        </a>
+                        <FontAwesomeIcon icon={faHeart} /> <span>{player.likes}</span>
+                    </div>
+                )
+            }
+    }
+
+    return (
+     renderPlayers()
+    )
+      
 }
 
 export default withRouter(PlayerInfoButtons);
