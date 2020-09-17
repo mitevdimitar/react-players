c842bff7d1a55d70273bce8acd7d9ebab502f50e
mitevdimitar <mitev.dimitar@gmail.com>
2020-09-17 08:52:04 +0300

    button styled with makeStyles

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,9 +1,27 @@
 import React from "react";
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginRight: "10px",
+        padding: "0.6rem 0.7re",
+        marginBottom: "0.7rem",
+        borderRadius: "0.4rem",
+        fontWeight: "bold",
+        background: "#234465",
+        color: "rgba(237, 244, 253, 0.966)",
+        "&:hover": {
+            background: "rgb(248, 215, 107)",
+            color: "rgb(0, 0, 0)"
+        }
+    },
+  }));
 
 function ButtonComponent(props) {
+    const classes = useStyles();
     return (
-        <Button onClick={() => props.handle()}>
+        <Button className={classes.root} variant="contained" onClick={() => props.handle()}>
             {props.children}
         </Button>
     )
