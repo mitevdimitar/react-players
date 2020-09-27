dab8904663ae68eabd11e4168a62fc9515e17924
mitevdimitar <mitev.dimitar@gmail.com>
2020-09-27 17:24:45 +0300

    prettier installed and configured

--- src/Header/Navbar/NavbarLogged.js
+++ src/Header/Navbar/NavbarLogged.js
@@ -1,66 +1,84 @@
 import React from 'react';
-import firebase from '../../Services/firebase'
+import firebase from '../../Services/firebase';
 import { Link, Redirect } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 class NavbarLogged extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: null,
+    };
+    this.handleUserLogout = this.handleUserLogout.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state={
-            redirect: null
-        };
-        this.handleUserLogout = this.handleUserLogout.bind(this);
-    }
+  handleUserLogout(e) {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        this.setState({ redirect: '/' });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-    handleUserLogout(e) {
-        e.preventDefault();
-        firebase
-        .auth()
-        .signOut()
-        .then(() => {
-            this.setState({ redirect: "/" })
-          }).catch(function(error) {
-            console.log(error)
-          });
-    }
+  toggleMenu(e) {
+    e.preventDefault();
+    let element = document.querySelectorAll('.navbar .first-bar .button');
+    let elementsArr = Array.apply(null, element);
+    elementsArr.map((a) => {
+      a.style.display === 'flex'
+        ? (a.style.display = 'none')
+        : (a.style.display = 'flex');
+    });
+  }
 
-    toggleMenu(e) {
-        e.preventDefault();
-        let element = document.querySelectorAll(".navbar .first-bar .button");
-        let elementsArr = Array.apply(null, element);
-        elementsArr
-            .map(a => {
-                a.style.display === "flex" ? a.style.display = "none" : a.style.display = "flex"
-            })
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />;
     }
-
-    render() {
-        if (this.state.redirect) {
-            return <Redirect to={this.state.redirect} />
-        }
-        return (
-                <section className="navbar-dashboard">
-                    <div className="nav-first-line">
-                        <div className="first-bar">
-                            <a href="#" onClick={this.toggleMenu} className="white"><FontAwesomeIcon icon={faBars} /></a>
-                            <Link className="button" to="/dashboard">Rankings</Link>
-                            <Link className="button" to="/myplayers">My Players</Link>
-                            <Link className="button" to="/addplayer">Add Player</Link>
-                            <Link className="button" to="/shop">Fanshop</Link>
-                        </div>
-                        <div className="second-bar">
-                            <ul>
-                                <li>Welcome!</li>
-                                <li><Link className="nav-button" to="/logout" onClick={this.handleUserLogout}><i className="fas fa-sign-out-alt"></i> Logout</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </section>
-        )
-    }
-    
+    return (
+      <section className="navbar-dashboard">
+        <div className="nav-first-line">
+          <div className="first-bar">
+            <a href="#" onClick={this.toggleMenu} className="white">
+              <FontAwesomeIcon icon={faBars} />
+            </a>
+            <Link className="button" to="/dashboard">
+              Rankings
+            </Link>
+            <Link className="button" to="/myplayers">
+              My Players
+            </Link>
+            <Link className="button" to="/addplayer">
+              Add Player
+            </Link>
+            <Link className="button" to="/shop">
+              Fanshop
+            </Link>
+          </div>
+          <div className="second-bar">
+            <ul>
+              <li>Welcome!</li>
+              <li>
+                <Link
+                  className="nav-button"
+                  to="/logout"
+                  onClick={this.handleUserLogout}
+                >
+                  <i className="fas fa-sign-out-alt"></i> Logout
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default NavbarLogged;
