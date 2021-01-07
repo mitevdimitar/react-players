9509bb794ce601f85c95f9bd2700a83d3d83e30c
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-01-07 09:12:41 +0200

    shopping cart buttons

--- src/Fanshop/ShopCart.js
+++ src/Fanshop/ShopCart.js
@@ -2,17 +2,24 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import CartRow from "./CartRow";
 import TotalRow from "./TotalRow";
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { ProductConsumer } from '../ContextWrapper';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      height: "75vh",
+      height: "75vh"
     },
     cart: {
       width: "70%",
-      minHeight: "300px",
-      background: "#234465"
+      minHeight: "320px",
+      background: "#234465",
+      position: "relative"
+    },
+    buttonContainer: {
+      position: "absolute",
+      bottom: 0,
+      right: "50px"
     }
   }));
 
@@ -28,6 +35,10 @@ function ShopCart() {
               <Grid container item justify="center" alignItems="center" direction="column" className={classes.cart}>
                 {products.map(product=>  <CartRow key={product.id} product={product} quantityChange={quantityChange} />)}
                 <TotalRow products={products}/>
+                <Grid container item justify="flex-end" className={classes.buttonContainer}>
+                  <Link to="/shop" className="details-button">Continue shopping</Link>
+                  <Link to="/" className="details-button">Checkout</Link>
+                </Grid>
               </Grid>
             </Grid>
           )
