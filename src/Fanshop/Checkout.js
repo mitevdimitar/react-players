15fa1aa9acb45a7247591e9a85e258d2a310dd06
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-01-14 09:00:55 +0200

    fireworks on checkout

--- src/Fanshop/Checkout.js
+++ src/Fanshop/Checkout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+import { Fireworks } from 'fireworks/lib/react'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,9 +14,21 @@ const useStyles = makeStyles((theme) => ({
 function Checkout() {
     const classes = useStyles();
 
+    const fxProps = {
+      count: 3,
+      interval: 200,
+      colors: ['#cc3333', '#4CAF50', '#81C784'],
+      calc: (props, i) => ({
+        ...props,
+        x: (i + 1) * (window.innerWidth / 3) - (i + 1) * 100,
+        y: 200 + Math.random() * 100 - 50 + (i === 2 ? -80 : 0)
+      })
+    }
+
     return(
         <Grid container justify="center" alignItems="center" className={classes.root}>
-            Congratulations! Your virtual purchase was successful! Thank you for trying My Favorite PL Player! 
+            <Fireworks {...fxProps} />
+            <div>Congratulations! Your virtual purchase was successful! Thank you for trying My Favorite PL Player! </div>
         </Grid>
     )
 }
