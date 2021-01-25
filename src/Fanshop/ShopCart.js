4745850993d5674eb23c310a05ea5de7ca3f66aa
Dimitar Mitev <mitev.dimitar@gmail.com>
2021-01-25 09:11:50 +0200

    show empty cart message if no products are added

--- src/Fanshop/ShopCart.js
+++ src/Fanshop/ShopCart.js
@@ -5,10 +5,11 @@ import TotalRow from "./TotalRow";
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { ProductConsumer } from '../ContextWrapper';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      height: ({inNavbar})=> (!inNavbar && "75vh")
+      height: ({inNavbar})=> (!inNavbar ? "75vh" : "100%")
     },
     cart: {
       width: ({inNavbar})=> (inNavbar ? "100%" : "70%"),
@@ -21,6 +22,9 @@ const useStyles = makeStyles((theme) => ({
       position: "absolute",
       bottom: 0,
       right: "50px"
+    },
+    emptyCartContainer: {
+      height: "100%"
     }
   }));
 
@@ -31,9 +35,11 @@ function ShopCart({inNavbar, setAnchorEl}) {
       <ProductConsumer>
         {(value) => {
           const {products, quantityChange, emptyCart} = value;
+          const productCounts = products.map(product=>product.count);
+          const productsInCart = productCounts.reduce((a, b) => a + b, 0);
           return (
             <Grid container justify="center" alignItems="center" className={classes.root}>
-              <Grid container item justify="center" alignItems="center" direction="column" className={classes.cart}>
+              {productsInCart > 0 ? <Grid container item justify="center" alignItems="center" direction="column" className={classes.cart}>
                 {products.map(product=>  <CartRow key={product.id} product={product} quantityChange={quantityChange} />)}
                 <TotalRow products={products}/>
                 <Grid container item justify="flex-end" className={classes.buttonContainer}>
@@ -50,6 +56,12 @@ function ShopCart({inNavbar, setAnchorEl}) {
                   }
                 </Grid>
               </Grid>
+              : 
+              <Grid className={classes.emptyCartContainer} container item alignItems="center" justify="center" direction="column">
+                <Typography>Cart it empty!</Typography>
+                <Typography>Add products from fanshop!</Typography>
+              </Grid>
+              }
             </Grid>
           )
         }}
